ab50d3142186a6c8877da9145118e62b7f361871
Amal Dusalin <adusalin@fil-it.ru>
2022-01-12 23:43:02 +0300

    Отправка ответа

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
 
 const app = express();
 
-app.use(async (request, response, next) => {
+app.use("/home/bar/foo", (request, response) => {
 
-    const date = new Date()
-
-    const logData = `${date.toTimeString()} ${request.method} ${request.url} ${request.get("user-agent")} \n`;
-
-    fs.appendFile(path.resolve(__dirname, "log.txt"), logData, next)
-})
-
-app.get("/", (request, response) => {
-
-    response.send("<h2>Hello express</h2>")
+    response.status(404).send("Nothing to do here!")
 });
 
 app.listen(3000);
