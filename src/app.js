9fafcffc5557b1436eb4b0db388c5b360871c7fb
Amal Dusalin <adusalin@fil-it.ru>
2022-01-13 00:48:52 +0300

    Post запросы и обработка форм

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+// создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = express.urlencoded({extended: false});
+
 app.get("/", (request, response) => {
 
-    response.response("<h2>Главная страница</h2>");
+    response.sendFile(path.resolve(__dirname, "index.html"))
 })
 
-app.use("/about", (request, response) => {
+app.post("/", urlencodedParser, (request, response) => {
 
-    const id = request.query.id;
-    const name = request.query.name;
+    if (!request.body) {
 
-    response.send({id, name})
+        return response.sendStatus(400)
+    }
+
+    response.send(request.body)
 })
 
 app.listen(3000);
