c067eb954b2a1da3c66ddfd61872573069438471
Amal Dusalin <adusalin@fil-it.ru>
2022-01-12 23:59:06 +0300

    Маршрутизация

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "public")));
+app.get("/bo?k", (request, response) => {
 
-app.get("/", (request, response) => {
+    response.send(request.url)
+})
 
-    response.send("<h1>Главная</h1>")
+app.get("/bo+k", (request, response) => {
+
+    response.send(request.url)
+})
+
+app.get("/bo*k", (request, response) => {
+
+    response.send(request.url)
 })
 
 app.listen(3000);
