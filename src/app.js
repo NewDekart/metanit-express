c1e03c5b50ae6ad4240f99d428020b0c60a8ab3b
Amal Dusalin <adusalin@fil-it.ru>
2022-01-12 23:22:50 +0300

    Конвеер обработки запроса и middleware

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
+
+app.use(async (request, response, next) => {
+
+    const date = new Date()
+
+    const logData = `${date.toTimeString()} ${request.method} ${request.url} ${request.get("user-agent")} \n`;
+
+    fs.appendFile(path.resolve(__dirname, "log.txt"), logData, next)
+})
 
 app.get("/", (request, response) => {
 
     response.send("<h2>Hello express</h2>")
-});
-
-app.get("/about", (request, response) => {
-
-    response.send("<h2>О сайте</h2>")
-});
-
-app.get("/contact", (request, response) => {
-
-    response.send("<h2>Контакты</h2>")
 });
 
 app.listen(3000);
